017f996d488ea13cfff496cec1d3f053c5359efd
Samarth Kumar <skuminater@gmail.com>
2022-01-15 18:20:27 -0500

    Edit styling

--- js/animations.js
+++ js/animations.js
@@ -1,9 +1,9 @@
-// const stopButton = document.getElementById('stop');
+const stopButton = document.getElementById('stop');
 
-// stopButton.addEventListener('click', showTranscript = () => {
-//     if (document.getElementById('jumbotron').style.display === "none") {
-//       document.getElementById('jumbotron').style.display = "";
-//     } else {
-//       document.getElementById('jumbotron').style.display = "none";
-//     }
-// })
+stopButton.addEventListener('click', showTranscript = () => {
+    if (document.getElementById('jumbotron').style.display === "none") {
+      document.getElementById('jumbotron').style.display = "";
+    } else {
+      document.getElementById('jumbotron').style.display = "none";
+    }
+})
